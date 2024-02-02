3a299bc75fb8f64f6c5fc0b05cfdb3271aa7f3a1
Monal Barse <monal.barse.cer22@itbhu.ac.in>
2024-02-02 19:38:58 +0530

    Removed the password

--- Chat-App/backend/config/db.js
+++ Chat-App/backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const colors = require('colors');
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(`mongodb+srv://monalbarsemongo:Noobm%40ster69.@cluster1.tgeutud.mongodb.net/`);
+        const conn = await mongoose.connect(``);
         console.log(`MongoDB Connected: ${conn.connection.host}`.blue.underline.bold);
     } catch (err) {
         console.error(`Error connecting to MongoDB: ${err.message}`);
